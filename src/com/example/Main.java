19b1dfc6b27c1216f7eecd431c06c32698693f87
Antal Sergiu-Alexandru <45335304+quavoatl@users.noreply.github.com>
2019-04-08 10:09:42 +0300

    Update Main.java

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -2,6 +2,7 @@ package com.example;
 
         import com.example.model.Artist;
         import com.example.model.Datasource;
+        import com.example.model.SongArtist;
 
         import java.util.List;
 
@@ -14,21 +15,31 @@ public class Main {
             return;
         }
 
-//        List<Artist> artists = datasource.queryArtist2(Datasource.ORDER_BY_DESC);
-//        if(artists == null) {
-//            System.out.println("no artists");
-//            return;
-//        }
-//        for(Artist artist : artists) {
-//            System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
-//        }
+        List<Artist> artists = datasource.queryArtist2(Datasource.ORDER_BY_DESC);
+        if(artists == null) {
+            System.out.println("no artists");
+            return;
+        }
+        for(Artist artist : artists) {
+            System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
+        }
 
-        List<String> albumsForArtist = datasource.queryAlbumsForArtist("Iron Maiden",Datasource.ORDER_BY_ASC);
+        List<String> albumsForArtist = datasource.queryAlbumsForArtist("Carole King",Datasource.ORDER_BY_NONE);
         for(String album : albumsForArtist) {
             System.out.println(album);
         }
 
+        List<SongArtist> artistForSong = datasource.queryArtistForSong("Go Your Own Way",Datasource.ORDER_BY_NONE);
 
+        for(SongArtist songArtist : artistForSong) {
+            if(songArtist == null) {
+                return;
+            }
+            System.out.println("Artist : " + songArtist.getArtistName() + ", Album : " + songArtist.getAlbumName()
+                                + ", Track : " + songArtist.getTrack());
+        }
+        
+        datasource.querySongsMetaData();
         datasource.close();
     }
 }
