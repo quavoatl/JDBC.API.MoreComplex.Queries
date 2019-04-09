87b5d20525de3a1542debef87e5afee555b64a57
Antal Sergiu-Alexandru <45335304+quavoatl@users.noreply.github.com>
2019-04-09 09:08:41 +0300

    Update Main.java

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -5,6 +5,7 @@ package com.example;
         import com.example.model.SongArtist;
 
         import java.util.List;
+        import java.util.Scanner;
 
 public class Main {
 
@@ -28,21 +29,38 @@ public class Main {
         for(String album : albumsForArtist) {
             System.out.println(album);
         }
-
+        
         List<SongArtist> artistForSong = datasource.queryArtistForSong("Go Your Own Way",Datasource.ORDER_BY_ASC);
 
         for(SongArtist songArtist : artistForSong) {
             if(songArtist == null) {
+                System.out.println("Could not find the artist for this song...");
                 return;
             }
             System.out.println("Artist : " + songArtist.getArtistName() + ", Album : " + songArtist.getAlbumName()
                                 + ", Track : " + songArtist.getTrack());
         }
-        
+
         System.out.println(Datasource.CREATE_VIEW_ARTIST_LIST);
-        datasource.createViewForArtist();    
-           
+        datasource.createViewForArtist();
+
         datasource.querySongsMetaData();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a song : ");
+        String sql = scanner.nextLine();
+
+        artistForSong = datasource.queryViewForSong(sql);
+
+        if(artistForSong.isEmpty()) {
+            System.out.println("Can't find what you are searching for...");
+            return;
+        }
+        for(SongArtist artistForSongg : artistForSong) {
+            System.out.println("FROM VIEW Artist : " + artistForSongg.getArtistName() + ", Album : " + artistForSongg.getAlbumName()
+                    + ", Track : " + artistForSongg.getTrack() );
+        }
+            
         datasource.close();
     }
 }
